5e67b64701370ef5e6c93eab0585e13f0dbe1f85
ronak suba <shrutiinfoaios@gmail.com>
2025-12-14 17:53:47 +0530

    gitignore added

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
 
   server: {
     host: "::",
+    allowedHost : "infoaios.ai",
     port: 3001,
         strictPort: true, // 👈 This line prevents Vite from switching to 8081
 
