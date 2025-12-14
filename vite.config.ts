c3a698fceada06f26e3240041c6182c77657254d
shrutiinfoaios-debug <shrutiinfoaios@gmail.com>
2025-12-14 06:45:42 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+export default defineConfig(({ mode }) => ({
+  base: "/",            // ✅ subdomain ke root pe app
+
+  server: {
+    host: "::",
+    port: 3001,
+        strictPort: true, // 👈 This line prevents Vite from switching to 8081
+
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
+
